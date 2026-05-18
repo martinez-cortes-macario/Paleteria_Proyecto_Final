33a04f32fb804aeb6ae090a635a21a8f435ca691
sanchezmedina67 <is3030364@gmail.com>
2026-05-18 16:54:48 -0700

    Se implementa la funcionalidad de inicio de sesión de usuarios mediante autenticación básica

--- store/views.py
+++ store/views.py
@@ -1,4 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
 
 def home(request):
-    return render(request, 'store/home.html')
+    products = Product.objects.all()
+    return render(request, 'store/home.html', {'products': products})
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'store/products.html', {'products': products})
+
+def product_detail(request, id):
+    product = get_object_or_404(Product, id=id)
+    return render(request, 'store/product_detail.html', {'product': product})
+
+def about(request):
+    return render(request, 'store/about.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
+    return render(request, 'store/login.html')
