e6bd1a79edc80a215ef600398b050aa19670061d
levigss <gutierrezsantiagojonathanlevigen2024@cbtis116.edu.mx>
2026-05-18 15:15:50 -0700

    Se configuran las rutas principales del proyecto y se conectan las vistas de la aplicación store

--- store/urls.py
+++ store/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('productos/', views.product_list, name='products'),
+    path('producto/<int:id>/', views.product_detail, name='product_detail'),
+    path('login/', views.login_view, name='login'),
+    path('about/', views.about, name='about'),
 ]
+
