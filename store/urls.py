c6706d7ef413592a525004520456abba74a89a26
martinez-cortes-macario <m23796376@gmail.com>
2026-05-25 02:44:53 -0700

    Se agrega rutas para funcionalidad de carrito y compra

--- store/urls.py
+++ store/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     path('producto/<int:id>/', views.product_detail, name='product_detail'),
     path('login/', views.login_view, name='login'),
     path('about/', views.about, name='about'),
+
+    path('cart/', views.cart_view, name='cart'),
+    path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('clear-cart/', views.clear_cart, name='clear_cart'),
+    path('success/', views.success, name='success'),
 ]
 
